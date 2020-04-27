ec485fd01349cd8c92876e82532b123b695b04b4
ernestoexposito <ernestoexposito@192.168.0.19>
2020-04-27 10:14:36 +0200

    This lab is intended to apply the EIP Messaging Integration Style pattern based on the use of the RabbitMQ Message Broker solution

---
+++ src/eip/fileintegration/mom/Consumer.java
@@ -0,0 +1,50 @@
+package eip.fileintegration.mom;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Consumer {
+// the name of the queue to be used to communicate
+
+    private final static String QUEUE_NAME = "queue1";
+
+    public static void main(String[] argv) throws Exception {
+        // Create a factory of connections to the MOM
+        ConnectionFactory factory = new ConnectionFactory();
+        // Specify the address of the MOM server
+        factory.setHost("localhost");
+        // Create a new connection to the MOM
+        Connection connection = factory.newConnection();
+        // Create a new channel within this connection
+        // to consume and to produce messages
+        Channel channel = connection.createChannel();
+        // Declare the queue to be used with the parameters:
+        // durable:false, exclusive: false, autodelete:false,
+        // additional-arguments:null
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // now we will be ready to receive messages
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        // a queue consumer is created on the chanel
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        // the consumer is associated is associate with the queue
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+        // receive and print the delivered messages
+            while (true) {
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        }
+}
---
+++ src/eip/fileintegration/mom/Producer.java
@@ -0,0 +1,46 @@
+package eip.fileintegration.mom;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Producer {
+// the name of the queue to be used to communicate
+
+    private final static String QUEUE_NAME = "queue1";
+
+    public static void main(String[] argv) throws Exception {
+// Create a factory of connections to the MOM
+        ConnectionFactory factory = new ConnectionFactory();
+// Specify the address of the MOM server
+        factory.setHost("localhost");
+// Create a new connection to the MOM
+        Connection connection = factory.newConnection();
+// Create a new channel within this connection
+// to consume and to produce messages
+        Channel channel = connection.createChannel();
+// Declare the queue to be used with the parameters:
+// durable:false, exclusive: false, autodelete:false,
+// additional-arguments:null
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+// Prepare the message
+        String message = "Hello World!";
+// Send the message to the queue
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+// Close the channel and the connection and finish the execution
+        channel.close();
+        connection.close();
+    }
+}
